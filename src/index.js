959aaac07d3af54e4a3ae2aad4830fcc7f08df51
Bruno Gomes <brunoo.gomes11@gmail.com>
2022-10-05 20:47:18 -0300

    improve base of middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,61 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User does not exists!' })
+  }
+  request.user = user;
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const isFree = user.pro === false;
+  const todosLength = user.todos.length;
+
+  if (isFree === true && todosLength >= 10) {
+    return response.status(403).json({ error: "Max todos Reached" }); 
+  } else {
+    return next()
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+  const isUUID = validate(id);
+  if(!isUUID){
+    return response.status(400).json({ error: "UUID not valid" })
+  }
+  const todoOfUser = user.todos.find(user => user.id === id);
+  if(!todoOfUser){
+    return response.status(404).json({ error: "Todo of user not found" })
+  }
+  if (user && isUUID && todoOfUser) {
+    request.user = user;
+    request.todo = todoOfUser;
+    return next()
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User does not exists!' })
+  }
+  request.user = user;
+  return next()
+
 }
 
 app.post('/users', (request, response) => {
